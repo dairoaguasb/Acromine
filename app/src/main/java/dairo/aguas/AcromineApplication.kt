42509429e1ea37f27bf8a9fc23a595b2af10c5fa
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-10-26 16:07:47 -0500

    Configure project

---
+++ app/src/main/java/dairo/aguas/AcromineApplication.kt
@@ -0,0 +1,11 @@
+package dairo.aguas
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Dairo Aguas B on 26/10/2021.
+ */
+
+@HiltAndroidApp
+class AcromineApplication : Application()
