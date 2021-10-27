1059c33c696cc7494cba52a2420064cd0ba2b82d
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-10-27 09:21:02 -0500

    Created AcromineAPI

---
+++ app/src/main/java/dairo/aguas/acromine/data/endpoints/AcromineAPI.kt
@@ -0,0 +1,16 @@
+package dairo.aguas.acromine.data.endpoints
+
+import dairo.aguas.acromine.data.model.AbbreviationDTO
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by Dairo Aguas B on 27/10/2021.
+ */
+interface AcromineAPI {
+
+    @GET(ABBREVIATION)
+    suspend fun getAbbreviationDefinitions(@Query("sf") abbreviation: String): AbbreviationDTO
+}
+
+private const val ABBREVIATION = "acromine/dictionary.py"
