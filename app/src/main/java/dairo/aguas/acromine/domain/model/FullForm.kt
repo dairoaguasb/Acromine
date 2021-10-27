949d1d343048f2c6bb9a6aea8b87ee8c7d7f2e54
Dairo Aguas B <ingdairoaguas@gmail.com>
2021-10-27 09:36:03 -0500

    Created domain models

---
+++ app/src/main/java/dairo/aguas/acromine/domain/model/FullForm.kt
@@ -0,0 +1,11 @@
+package dairo.aguas.acromine.domain.model
+
+/**
+ * Created by Dairo Aguas B on 27/10/2021.
+ */
+data class FullForm(
+    val freq: Int,
+    val lf: String,
+    val since: Int,
+    val variationObjects: List<VariationObjects>
+)
---
+++ app/src/main/java/dairo/aguas/acromine/domain/model/LongForms.kt
@@ -0,0 +1,9 @@
+package dairo.aguas.acromine.domain.model
+
+/**
+ * Created by Dairo Aguas B on 27/10/2021.
+ */
+data class LongForms(
+    val fullForms: List<FullForm>,
+    val sf: String
+)
